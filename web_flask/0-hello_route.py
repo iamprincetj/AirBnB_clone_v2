77bac362a09b53ce06735fa40fee3aa4a856c7ac
iamprincetj <tochukwunwanze5@gmail.com>
2023-07-21 18:27:28 +0000

    done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,6 +4,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.debug = False
 
 
 @app.route("/", strict_slashes=False)
@@ -12,4 +13,4 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
